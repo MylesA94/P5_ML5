93909bd81ba110616149b213ee0df03919c319d4
Myles Albaladejo <balatm45@gmail.com>
2020-12-30 12:05:20 -0800

    coad added to load in MobileNet model

--- image_classification/sketch.js
+++ image_classification/sketch.js
@@ -1,4 +1,12 @@
+let mobilenet;
+
+function modelReady() {
+  console.log("Model is ready!!!");
+}
+
 function setup() {
+  console.log("ml5 version:", ml5.version);
   createCanvas(640, 480);
   background(0);
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
