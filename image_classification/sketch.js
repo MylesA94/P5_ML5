7e3cccc17c991b1f1c2b53aff221f257f544c328
Myles Albaladejo <balatm45@gmail.com>
2020-12-30 12:13:46 -0800

    added image to canvas using imageReady function.

--- image_classification/sketch.js
+++ image_classification/sketch.js
@@ -1,12 +1,23 @@
 let mobilenet;
 
+let puffin;
+
 function modelReady() {
   console.log("Model is ready!!!");
+}
+
+function imageReady() {
+  image(puffin, 0, 0, width, height);
 }
 
 function setup() {
   console.log("ml5 version:", ml5.version);
   createCanvas(640, 480);
+  puffin = createImg(
+    "C:/Users/Myles Albaladejo/Pictures/IMG_5761.JPG",
+    imageReady
+  );
+  puffin.hide();
   background(0);
   mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
