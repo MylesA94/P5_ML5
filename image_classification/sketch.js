3f639a0cee04e24637130c986c3353031ea6be18
Myles Albaladejo <balatm45@gmail.com>
2021-01-04 17:30:01 -0800

    added implementation of webcam continuous video classification

--- image_classification/sketch.js
+++ image_classification/sketch.js
@@ -1,10 +1,14 @@
 let mobilenet;
 
 let puffin;
+let gorilla;
+let video;
+let label = "";
 
-function modelReady() {
+function modelReady(data) {
   console.log("Model is ready!!!");
-  mobilenet.classify(puffin, gotResults);
+  //mobilenet.classify(gorilla, gotResults);
+  mobilenet.classify(gotResults);
 }
 
 function gotResults(error, results) {
@@ -13,26 +17,41 @@ function gotResults(error, results) {
   } else {
     console.log(results);
     // get label of possible classifications
-    let label = results[0].label.split(",");
+    label = results[0].label.split(",");
     // get probability and make percentage for display
     let probability = results[0].confidence * 100;
-    fill(0);
-    textSize(64);
-    // display classification as well as probability of classification correctness
-    text(label[0].toUpperCase(), 10, height - 100);
-    text(`probability: ${probability.toFixed(2)}%`, 10, height - 180);
+    // // display classification as well as probability of classification correctness
+
+    // text(`probability: ${probability.toFixed(2)}%`, 10, height - 180);
+    mobilenet.classify(gotResults);
   }
 }
 
-function imageReady() {
-  image(puffin, 0, 0, width, height);
-}
+// function imageReady() {
+//   image(puffin, 0, 0, width, height);
+// }
+
+// function imageReady2() {
+//   image(gorilla, 0, 0, width, height);
+// }
 
 function setup() {
   console.log("ml5 version:", ml5.version);
   createCanvas(640, 480);
-  puffin = createImg("./images/puffin.jpg", imageReady);
-  puffin.hide();
+  // puffin = createImg("./images/puffin.jpg", imageReady);
+  // gorilla = createImg("./images/gorilla.jpg", imageReady2);
+  video = createCapture(VIDEO);
+  //gorilla.hide();
+  //puffin.hide();
+  video.hide();
   background(0);
-  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+  mobilenet = ml5.imageClassifier("MobileNet", video, modelReady);
+}
+
+function draw() {
+  background(0);
+  image(video, 0, 0);
+  fill(255);
+  textSize(32);
+  text(label, 10, height - 20);
 }
